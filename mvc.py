3bc42feff5092bb10ca09d2ccc122a131db1d173
nedfla <camendez632@ucatolica.edu.co>
2024-08-04 17:30:31 -0500

    profesores

--- mvc.py
+++ mvc.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
         print("1. Agregar Estudiantes")
         print("2. Mostrar Estudiantes")
         print("3. Mostrar Estudiante Mayor")
-        print("4. Agregar profesores")
-        print("5. Salir")
+        print("4. Agregar Profesores")
+        print("5. Mostrar Profesores")
+        print("6. Último Profesor Ingresado")
+        print("7. Salir")
         opcion = input("Seleccione una opción: ")
         if opcion == "1":
             controlador.agregar_estudiante()
@@ -18,8 +20,13 @@ if __name__ == "__main__":
         elif opcion == "3":
             controlador.estudiante_mayor()
         elif opcion == "4":
-            controlador.agregar.profesor()
+            controlador.agregar_profesor()
         elif opcion == "5":
+            controlador.mostrar_profesores()
+        elif opcion == "6":
+            controlador.ultimo_profesor_ingresado()
+        elif opcion == "7":
             break
         else:
             print("Opción no válida. Inténtelo de nuevo.")
+
